9aa521408086a0fc7963fc3f63ec9266515f80e1
nikolaspasojevictr <nikola.spasojevic@thomsonreuters.com>
2021-04-12 18:01:17 +0200

    updated config files

--- config/aws.py
+++ config/aws.py
@@ -1,3 +1,7 @@
+import boto3
+from sagemaker.amazon.amazon_estimator import get_image_uri
+from time import gmtime, strftime
+
 
 # aws profile
 profile = {
@@ -5,25 +9,40 @@ profile = {
     'aws_region': 'eu-west-1'
 }
 
+
 # workspace
 workspace_id = 'ModelDrift'
-role = ''
+role = 
+region = boto3.Session().region_name
+bucket = 'a204311-scw-euw1-modeldrift'
+
+
+# data capture
+prefix = 'sagemaker/DEMO-ModelMonitor'
+data_capture_prefix = '{}/datacapture'.format(prefix)
+s3_capture_upload_path = 's3://{}/{}'.format(bucket, data_capture_prefix)
+
 
 # endpoint
 endpoint_name = 'DEMO-xgb-churn-pred-model-monitor-2021-02-11-12-50-59'
 
+
 # custom analyzer image (for baselining and monitoing) config
 repository_arn = "arn:aws:ecr:eu-west-1:451191978663:repository/a204311-trlabs/model-monitor-baseline"
 registry_id = "451191978663"
-# baselining_repository_name = "a204311-trlabs/model-monitor-baseline"
 baselining_repository_name = 'model-monitor-baseline'
 baselining_repository_uri = "451191978663.dkr.ecr.eu-west-1.amazonaws.com/a204311-trlabs/model-monitor-baseline"
+
 
 # baselining file paths
 baseline_dataset_name = 'validation_with_predictions.csv'
 baseline_data_uri = 's3://a204311-scw-euw1-modeldrift/sagemaker/DEMO-ModelMonitor/baselining/data'
 baseline_results_uri = 's3://a204311-scw-euw1-modeldrift/sagemaker/DEMO-ModelMonitor/baselining/results'
 
-# statistics and constraints file paths
-statistics_file_s3_uri = 's3://a204311-scw-euw1-modeldrift/sagemaker/DEMO-ModelMonitor/baselining/results/statistics.json'
+
+# groundtruth labels path
+ground_truth_upload_path = 's3://a204311-scw-euw1-modeldrift/sagemaker/DEMO-ModelMonitor/ground_truth'
+
+
+# constraints file path
 constraints_file_s3_uri = 's3://a204311-scw-euw1-modeldrift/sagemaker/DEMO-ModelMonitor/baselining/results/constraints.json'
---
+++ config/env_var.py
@@ -0,0 +1,6 @@
+import config.aws as aws_config
+
+env = {
+    'THRESHOLD': '0.15',
+    'baseline_dataset_name': aws_config.baseline_dataset_name
+}
